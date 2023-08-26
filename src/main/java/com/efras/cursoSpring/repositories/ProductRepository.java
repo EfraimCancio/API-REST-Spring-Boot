acf0baa264f980dd602adf751f8e36a4675e4e5e
Efraim Cancio da Fonseca <efraimfonseca@gmail.com>
2023-08-25 23:08:23 -0300

    Termino do curso

---
+++ src/main/java/com/efras/cursoSpring/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.efras.cursoSpring.repositories;
+
+import com.efras.cursoSpring.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
